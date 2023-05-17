c7d60f8b3197f16638d21b58d10e9290bcb20b16
GianfrancoBotta <124842306+GianfrancoBotta@users.noreply.github.com>
2023-05-17 17:07:39 +0200

    Add files via upload

--- patch_generator.py
+++ patch_generator.py
@@ -20,6 +20,7 @@ def patch_generator(images_path: str, masks_path: str, dataset_name: str, out_di
   import numpy as np
   from SEGMENTATION.classes import MonusacDataset
   from SEGMENTATION.patch_extractor import PatchExtractor
+  from hover_net.misc.utils import rm_n_mkdir
 
   dataset = MonusacDataset(images_path, masks_path)
   save_root = '/content/patches'
