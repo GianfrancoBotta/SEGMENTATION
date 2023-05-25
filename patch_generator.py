fixed image channels in normalisation

patch_generator.py
@@ -29,6 +29,10 @@ def patch_generator(images_path: str, masks_path: str, dataset_name: str, out_di
   save_root_black = '/content/black_patches'
   parser = MonusacDataset
   xtractor = PatchExtractor(win_size, step_size)
+  if blue_chan:
+    img_channels = 1
+  else:
+    img_channels = 3
 
   #create patches' directory
   out_dir = '%s/%s/%s/%dx%d_%dx%d/' % (
@@ -97,8 +101,8 @@ def patch_generator(images_path: str, masks_path: str, dataset_name: str, out_di
       random.shuffle(patches)
 
       for idx, patch in enumerate(patches):
-        patch_norm = cv2.normalize(patch[:,:,:3], None, 0, 1.0, cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-        patch_masks = patch[:,:,3:]
+        patch_norm = cv2.normalize(patch[:,:,:img_channels], None, 0, 1.0, cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+        patch_masks = patch[:,:,img_channels:]
         patch = np.concatenate((patch_norm, patch_masks), axis=-1)
         np.save("{0}/{1}_{2:03d}.npy".format(out_dir, base_name, idx), patch)
         pbar.update()
@@ -109,8 +113,8 @@ def patch_generator(images_path: str, masks_path: str, dataset_name: str, out_di
 
     else:
       for idx, patch in enumerate(sub_patches):
-        patch_norm = cv2.normalize(patch[:,:,:3], None, 0, 1.0, cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-        patch_masks = patch[:,:,3:]
+        patch_norm = cv2.normalize(patch[:,:,:img_channels], None, 0, 1.0, cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+        patch_masks = patch[:,:,img_channels:]
         patch = np.concatenate((patch_norm, patch_masks), axis=-1)
         np.save("{0}/{1}_{2:03d}.npy".format(out_dir, base_name, idx), patch)
         pbar.update()
@@ -119,8 +123,8 @@ def patch_generator(images_path: str, masks_path: str, dataset_name: str, out_di
       pbarx.update()
       pbarx.close()
       for idx, patch in enumerate(black_patches):
-        patch_norm = cv2.normalize(patch[:,:,:3], None, 0, 1.0, cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-        patch_masks = patch[:,:,3:]
+        patch_norm = cv2.normalize(patch[:,:,:img_channels], None, 0, 1.0, cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+        patch_masks = patch[:,:,img_channels:]
         patch = np.concatenate((patch_norm, patch_masks), axis=-1)
         np.save("{0}/{1}_{2:03d}.npy".format(out_dir_black, base_name, idx), patch)
         pbar.update()